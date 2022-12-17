43b07a3cccf223bade9d3debf8e3bcf5f50db148
Mert Aktaş <57454282+mrtaakts@users.noreply.github.com>
2022-12-18 02:55:08 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CategoryDto
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
