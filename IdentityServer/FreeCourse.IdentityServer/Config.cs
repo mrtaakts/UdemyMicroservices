bb423fb863d88f0c684a19550c1d9927eae4d960
Mert Aktaş <57454282+mrtaakts@users.noreply.github.com>
2022-12-24 22:09:45 +0300

    Added BasketAPI

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -17,6 +17,7 @@ namespace FreeCourse.IdentityServer
             {
                 new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
                 new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
+                new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
                 new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -34,6 +35,7 @@ namespace FreeCourse.IdentityServer
             {
                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
+               new ApiScope("basket_fullpermission","Basket API için full erişim"),
                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -55,7 +57,7 @@ namespace FreeCourse.IdentityServer
                     ClientSecrets={new Secret("secret".Sha256())},
                     AllowOfflineAccess=true,
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
+                    AllowedScopes = {"basket_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
                          IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess,IdentityServerConstants.LocalApi.ScopeName,"roles" },
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,36 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            if (string.IsNullOrEmpty(existBasket)) return Response<BasketDto>.Fail("Basket not found",404);
+
+            var basketDto = JsonSerializer.Deserialize<BasketDto>(existBasket);
+            return Response<BasketDto>.Success(basketDto, 200);
+        }
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var basket = JsonSerializer.Serialize(basketDto);
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserId, basket);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket could not update or save", 500);
+        }
+        public async Task<Response<bool>> Delete(string userId)
+        {
+            var status = await _redisService.GetDb().KeyDeleteAsync(userId);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket not found", 404);
+        }
+    }
+}
