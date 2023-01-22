4e9a76a502c67e1b4dab0816715ac5d517c0bab5
Mert Aktaş <57454282+mrtaakts@users.noreply.github.com>
2023-01-22 23:51:15 +0300

    Frontend implementation -1

--- Frontends/FreeCourse.Web/Program.cs
+++ Frontends/FreeCourse.Web/Program.cs
@@ -1,14 +1,32 @@
+using FreeCourse.Web.Handler;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services;
 using FreeCourse.Web.Services.Interface;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddHttpClient<IIdentityService, IdentityService>();
-builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
+builder.Services.AddHttpContextAccessor();
+var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
+builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+builder.Services.AddHttpClient<IIdentityService, IdentityService>();
 
-// Add services to the container.
+builder.Services.AddHttpClient<IUserService, UserService>(opt =>
+{
+    opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
+}).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opts =>
+{
+    opts.LoginPath = "/Auth/SignIn";
+    opts.ExpireTimeSpan = TimeSpan.FromDays(60);
+    opts.SlidingExpiration = true;
+    opts.Cookie.Name = "udemywebcookie";
+});
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,6 +40,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
